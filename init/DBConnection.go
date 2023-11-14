db2f06383c6cbd35b0cda859ccdc20dcfa30b15c
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2023-11-14 20:04:12 +0100

    connect the app to postgres and migrate data

---
+++ init/DBConnection.go
@@ -0,0 +1,24 @@
+package initializer
+
+import (
+	"EtsyScraper/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DataBaseConnect(config *Config) {
+
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", config.DataBaseHost, config.DataBaseUserName, config.DatabaseUserPassword, config.DataBaseName, config.DataBasePort)
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("a problem accured while trying to connect to the Database")
+	}
+	fmt.Println("Successfully connected to the database")
+	DB.AutoMigrate(&models.Account{})
+}
