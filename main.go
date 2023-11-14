c6336473e6feb5c19bfdf464fdb82258fe2fefc0
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2023-11-14 20:07:09 +0100

    setup Gin Gonic server

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	initializer "EtsyScraper/init"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"fmt"
+	"log"
+)
+
+var server *gin.Engine
+
+func init() {
+	config, err := initializer.LoadProjConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+
+	initializer.DataBaseConnect(&config)
+}
 func main() {
 
+	config, err := initializer.LoadProjConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+
+	}
+
+	server = gin.Default()
+	fmt.Println("Migration is completed")
+
+	router := server.Group("/auth")
+	router.GET("/test", func(ctx *gin.Context) {
+		message := "Welcome to EtsyScraper a"
+		ctx.JSON(http.StatusOK, gin.H{"HTTPstatus": http.StatusOK, "message": message})
+	})
+
+	log.Fatal(server.Run(":" + config.ServerPort))
 }
