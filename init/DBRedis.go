2c674dae74d3ef9f258e52fe8921c32342dcded3
Wael Abu ahmad <waelabuahmad@Waels-MacBook-Air.local>
2023-12-05 16:25:50 +0100

    upgrated to redis/v8 package

--- init/DBRedis.go
+++ init/DBRedis.go
@@ -1,18 +1,22 @@
 package initializer
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 )
 
+var RedisClient *redis.Client
+
 func RedisDBConnect(config *Config) {
-	redisClient := redis.NewClient(&redis.Options{
+	context := context.TODO()
+	RedisClient = redis.NewClient(&redis.Options{
 		Addr:     config.RedisURL,
 		Password: "",
 		DB:       0,
 	})
-	response, err := redisClient.Ping().Result()
+	response, err := RedisClient.Ping(context).Result()
 	if err != nil {
 		fmt.Println(response)
 	}
