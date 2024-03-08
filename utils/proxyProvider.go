8c980b39085340fb5c4e484a5412571125b02539
Wael Abu ahmad <waelabuahmad@Waels-MacBook-Air.local>
2024-03-08 16:00:41 +0100

    random proxyprovider and random country

--- utils/proxyProvider.go
+++ utils/proxyProvider.go
@@ -4,10 +4,12 @@ import (
 	initializer "EtsyScraper/init"
 	"fmt"
 	"math/rand"
+	"strings"
 )
 
 var config = initializer.LoadProjConfig(".")
 var GetAllEnvProy = []string{config.ProxyHostURL1, config.ProxyHostURL2, config.ProxyHostURL3}
+var Countries = []string{"UK", "FR", "DE", "US", "IR", "IT", "SP"}
 
 type ProxySetting struct {
 	Provider string
@@ -19,8 +21,11 @@ func PickProxyProvider() ProxySetting {
 
 	RandomIndex := rand.Intn(len(GetAllEnvProy))
 
-	ProxySettings.Provider = fmt.Sprint("Provider ", RandomIndex+1)
-	ProxySettings.Url = GetAllEnvProy[RandomIndex]
+	CountryList := strings.Split(GetAllEnvProy[RandomIndex], ";")
+	Country := rand.Intn(len(CountryList))
+
+	ProxySettings.Provider = fmt.Sprint("Provider ", RandomIndex+1, " Country :", Countries[Country])
+	ProxySettings.Url = CountryList[Country]
 
 	return ProxySettings
 }
