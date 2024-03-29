79b2b8da352eb4ee342d808de0601c4cd51ed3d4
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2024-03-29 22:29:14 +0100

    changes file name from verifyEmail.go to Email.go , added SendResetPassEmail() so create and sends the user a email with endpoint /reset_password

--- utils/Emails.go
+++ utils/Emails.go
@@ -36,7 +36,6 @@ func SendVerificationEmail(account *models.Account) error {
 
 	Config := initializer.LoadProjConfig("/")
 
-	// verificationLink := Config.ClientOrigin + "/verifyaccount?TranID=" + url.QueryEscape(account.EmailVerificationToken)
 	verificationLink, err := url.Parse(Config.ClientOrigin)
 	if err != nil {
 		log.Fatal(err)
@@ -70,6 +69,48 @@ func SendVerificationEmail(account *models.Account) error {
 	err = e.Send(fmt.Sprintf("%s:%v", Config.SMTPHost, Config.SMTPPort), smtp.PlainAuth(Config.EmailAddress, Config.SMTPUser, Config.SMTPPass, Config.SMTPHost))
 	if err != nil {
 		fmt.Println("There was an error sending the mail", err)
+	}
+	return nil
+}
+
+func SendResetPassEmail(account *models.Account) error {
+
+	Config := initializer.LoadProjConfig("/")
+
+	verificationLink, err := url.Parse(Config.ClientOrigin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	verificationLink.Path += "/reset_password"
+	param := url.Values{}
+	param.Add("rcp", account.AccountPassResetToken)
+	verificationLink.RawQuery = param.Encode()
+
+	e := &email.Email{
+		To:      []string{account.Email},
+		From:    Config.EmailAddress,
+		Subject: "Reset Password",
+		Text:    []byte("This is an email sent upon your request to reset your account password at EtsyScraper. if you did not request this email then please igonre the message, otherwise please press on the button to reset your password"),
+		HTML: []byte(fmt.Sprintf(`<html>
+		<head>
+		<div>
+		<h1>Hello %s,</h1>
+		<p>This is an email sent upon your request to reset your account password at EtsyScraper </p>
+		<p>if you did not request this email then please igonre the message, otherwise please press on the button to reset your password.  </p>
+		<button>
+		<a href="%s">Reset Password</a>
+		</button>
+		<p>The EtsyScraper Team</p>
+	  	</div>
+	 </body>
+		</html>`, account.FirstName, verificationLink.String())),
+		Headers: textproto.MIMEHeader{},
+	}
+
+	err = e.Send(fmt.Sprintf("%s:%v", Config.SMTPHost, Config.SMTPPort), smtp.PlainAuth(Config.EmailAddress, Config.SMTPUser, Config.SMTPPass, Config.SMTPHost))
+	if err != nil {
+		log.Println("There was an error sending the mail", err)
+		return err
 	}
 	return nil
 }
