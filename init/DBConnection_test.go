935613dd2af983bc4dbdf2743888b1a596d2a141
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2024-04-18 16:51:35 +0200

    tests for initializer package

---
+++ init/DBConnection_test.go
@@ -0,0 +1,23 @@
+package initializer_test
+
+import (
+	initializer "EtsyScraper/init"
+	"testing"
+)
+
+func TestValidConfiguration_ConnectionSuccess(t *testing.T) {
+
+	config := initializer.LoadProjConfig("../")
+
+	initializer.DataBaseConnect(&config)
+
+	if initializer.DB == nil {
+		t.Error("DB is nil after successful connection")
+	}
+
+	sqlDB, err := initializer.DB.DB()
+	if err != nil {
+		t.Fatalf("failed to get database instance: %v", err)
+	}
+	sqlDB.Close()
+}
