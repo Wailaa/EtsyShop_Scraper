dcc068584309bec5abc9e19964f1c1b9e825a34b
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2024-05-04 22:08:20 +0200

    implemented GeneralShopRoutes() in main()

--- main.go
+++ main.go
@@ -48,30 +48,10 @@ func main() {
 	implShop := controllers.Shop{DB: initializer.DB, Scraper: Scraper, Process: Creatrors}
 
 	userRoutes := routes.NewUserRouteController(controllers.NewUserController(initializer.DB, utils))
-
 	userRoutes.GeneraluserRoutes(server, controllers.AuthMiddleWare(utils), controllers.Authorization())
 
-	shopRoute := server.Group("/shop")
-	createNewShopRequest := controllers.NewShopController(implShop).CreateNewShopRequest
-	followShop := controllers.NewShopController(implShop).FollowShop
-	unFollowShop := controllers.NewShopController(implShop).UnFollowShop
-	getShopByID := controllers.NewShopController(implShop).HandleGetShopByID
-	getAllItemsByShopID := controllers.NewShopController(implShop).HandleGetItemsByShopID
-	getAllSoldItemsByShopID := controllers.NewShopController(implShop).HandleGetSoldItemsByShopID
-	getShopStats := controllers.NewShopController(implShop).ProcessStatsRequest
-	getItemsCountByShopID := controllers.NewShopController(implShop).HandleGetItemsCountByShopID
-
-	shopRoute.GET("/create_shop", controllers.AuthMiddleWare(utils), controllers.Authorization(), createNewShopRequest)
-	shopRoute.GET("/follow_shop", controllers.AuthMiddleWare(utils), controllers.Authorization(), followShop)
-	shopRoute.GET("/unfollow_shop", controllers.AuthMiddleWare(utils), controllers.Authorization(), unFollowShop)
-	shopRoute.GET("/:shopID", controllers.AuthMiddleWare(utils), controllers.Authorization(), getShopByID)
-	shopRoute.GET("/:shopID/all_items", controllers.AuthMiddleWare(utils), controllers.Authorization(), getAllItemsByShopID)
-
-	shopRoute.GET("/:shopID/all_sold_items", controllers.AuthMiddleWare(utils), controllers.Authorization(), getAllSoldItemsByShopID)
-
-	shopRoute.GET("/:shopID/items_count", controllers.AuthMiddleWare(utils), controllers.Authorization(), getItemsCountByShopID)
-
-	shopRoute.GET("/stats/:shopID/:period", controllers.AuthMiddleWare(utils), controllers.Authorization(), getShopStats)
+	shopRoutes := routes.NewShopRouteController(&implShop)
+	shopRoutes.GeneralShopRoutes(server, controllers.AuthMiddleWare(utils), controllers.Authorization())
 
 	server.Static("/static", "./static")
 	server.LoadHTMLGlob("static/templates/*")
