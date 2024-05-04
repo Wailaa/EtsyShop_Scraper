bf94268ac0a017b272a2ffa1b6afd0e2e925192d
Wael Abu ahmad <waelabuahmad@Waels-Air.fritz.box>
2024-05-05 00:21:19 +0200

    implemented GeneralHTMLRoutes in main()

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	scheduleUpdates "EtsyScraper/scheduleUpdateTask"
 	scrap "EtsyScraper/scraping"
 	"EtsyScraper/utils"
-	"net/http"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -42,6 +41,7 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+
 	utils := &utils.Utils{}
 	Scraper := &scrap.Scraper{}
 	Creatrors := &controllers.ShopCreators{DB: initializer.DB}
@@ -53,26 +53,9 @@ func main() {
 	shopRoutes := routes.NewShopRouteController(&implShop)
 	shopRoutes.GeneralShopRoutes(server, controllers.AuthMiddleWare(utils), controllers.Authorization())
 
-	server.Static("/static", "./static")
-	server.LoadHTMLGlob("static/templates/*")
-	server.GET("/reset_password", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "resetPass.html", nil)
-	})
-	server.GET("/change_password", controllers.AuthMiddleWare(utils), controllers.Authorization(), func(c *gin.Context) {
-		c.HTML(http.StatusOK, "changePass.html", nil)
-	})
-	server.GET("/log_in", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "logIn.html", nil)
-	})
-	server.GET("/verify_account", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "verifyAccount.html", nil)
-	})
-	server.GET("/stats", controllers.AuthMiddleWare(utils), controllers.Authorization(), func(c *gin.Context) {
-		c.HTML(http.StatusOK, "stats.html", nil)
-	})
-	server.GET("/", controllers.AuthMiddleWare(utils), controllers.Authorization(), func(c *gin.Context) {
-		c.HTML(http.StatusOK, "mainPage.html", nil)
-	})
+	templatesFilesPath := "./static/templates/*"
+	htmlRoutes := routes.NewHTMLRouter()
+	htmlRoutes.GeneralHTMLRoutes(server, controllers.AuthMiddleWare(utils), controllers.Authorization(), templatesFilesPath)
 
 	log.Fatal(server.Run(":" + config.ServerPort))
 
